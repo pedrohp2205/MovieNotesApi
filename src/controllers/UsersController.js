eaacf19385ee42b7f3432ac4c7dc0b1e42efc3aa
Pedro <hpm1819@gmail.com>
2024-08-26 21:41:59 -0300

    Back End concluido.

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,70 @@
+const AppError = require("../utils/AppError")
+const { hash, compare } = require("bcryptjs");
+const knex = require("../database/knex")
+
+class UsersController {
+    async create(request, response) {
+        const { name, email, password } = request.body;
+
+        const checkUserExists = await knex("users").where( { email } )
+
+        if(checkUserExists.length > 0 ) {
+            console.log(checkUserExists)
+            throw new AppError ("Este email ja está em uso.")
+        }
+
+        await knex("users").insert({
+            name,
+            email,
+            password
+        })
+
+        return response.status(201).json("");
+    }
+
+
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const user = await knex("users").where( { id } ).first()
+
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado.")
+        }   
+
+        const userWithUpdatedEmail = await knex("users").where( {email} ).first()
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Email já em uso")
+        }
+
+
+
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha.")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword) {
+                throw new AppError("As senhas não batem")
+            }
+
+            user.password = await hash(password, 8 )
+        }
+
+
+        await knex("users").where({ id }).update({
+            name: name || user.name,
+            email: email || user.email,
+            password: user.password
+        }, ['id', 'name', 'email', 'password'])
+
+        return response.json()
+    }
+}
+
+module.exports = UsersController
